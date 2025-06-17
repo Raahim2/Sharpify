e5940e2b626abf3e5fb0b5b0d513a68dd0660170
Raahim <127599591+Raahim2@users.noreply.github.com>
2025-06-17 11:54:11 +0530

    Update index.py

--- TestAPI/api/index.py
+++ TestAPI/api/index.py
@@ -5,14 +5,3 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def hello():
     return jsonify({"message": "Hello from Flask on Vercel!"})
-
-# Vercel will call this handler
-def handler(event, context):
-    from werkzeug.middleware.dispatcher import DispatcherMiddleware
-    from werkzeug.wrappers import Request, Response
-
-    def simple_app(environ, start_response):
-        return Response("Not Found", status=404)(environ, start_response)
-
-    app_full = DispatcherMiddleware(simple_app, {"/": app})
-    return app_full(environ=event["environ"], start_response=event["start_response"])
