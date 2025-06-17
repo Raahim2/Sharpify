30ada0c2776bcca663929ad6cf0961f23f3ee33f
Raahim <127599591+Raahim2@users.noreply.github.com>
2025-06-17 12:25:28 +0530

    Add files via upload

--- VercelAPI/main.py
+++ VercelAPI/main.py
@@ -3,10 +3,5 @@ from routes import image_routes
 
 app = FastAPI()
 
-# Include your image routes under /api
 app.include_router(image_routes.router, prefix="/api")
 
-# Root endpoint for /
-@app.get("/")
-async def root():
-    return {"message": "Hello from FastAPI on Vercel!"}
